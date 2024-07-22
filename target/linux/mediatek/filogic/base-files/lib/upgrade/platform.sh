a3f6b75cbad00ddb6950e95737e74118e494bfa9
DHDAXCW <lasstp5011@gmail.com>
2024-07-22 18:07:26 +0800

    mac80211: update to version 6.9.9

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -12,88 +12,23 @@ asus_initial_setup()
 	ubimkvol /dev/ubi0 -N jffs2 -s 0x3e000
 }
 
-xiaomi_initial_setup()
-{
-	# initialize UBI and setup uboot-env if it's running on initramfs
-	[ "$(rootfs_type)" = "tmpfs" ] || return 0
-
-	local mtdnum="$( find_mtd_index ubi )"
-	if [ ! "$mtdnum" ]; then
-		echo "unable to find mtd partition ubi"
-		return 1
-	fi
-
-	local kern_mtdnum="$( find_mtd_index ubi_kernel )"
-	if [ ! "$kern_mtdnum" ]; then
-		echo "unable to find mtd partition ubi_kernel"
-		return 1
-	fi
-
-	ubidetach -m "$mtdnum"
-	ubiformat /dev/mtd$mtdnum -y
-
-	ubidetach -m "$kern_mtdnum"
-	ubiformat /dev/mtd$kern_mtdnum -y
-
-	if ! fw_printenv -n flag_try_sys2_failed &>/dev/null; then
-		echo "failed to access u-boot-env. skip env setup."
-		return 0
-	fi
-
-	fw_setenv boot_wait on
-	fw_setenv uart_en 1
-	fw_setenv flag_boot_rootfs 0
-	fw_setenv flag_last_success 1
-	fw_setenv flag_boot_success 1
-	fw_setenv flag_try_sys1_failed 8
-	fw_setenv flag_try_sys2_failed 8
-
-	local board=$(board_name)
-	case "$board" in
-	xiaomi,mi-router-ax3000t|\
-	xiaomi,mi-router-wr30u-stock)
-		fw_setenv mtdparts "nmbm0:1024k(bl2),256k(Nvram),256k(Bdata),2048k(factory),2048k(fip),256k(crash),256k(crash_log),34816k(ubi),34816k(ubi1),32768k(overlay),12288k(data),256k(KF)"
-		;;
-	xiaomi,redmi-router-ax6000-stock)
-		fw_setenv mtdparts "nmbm0:1024k(bl2),256k(Nvram),256k(Bdata),2048k(factory),2048k(fip),256k(crash),256k(crash_log),30720k(ubi),30720k(ubi1),51200k(overlay)"
-		;;
-	esac
-}
-
 platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
-	acer,predator-w6|\
-	smartrg,sdg-8612|\
-	smartrg,sdg-8614|\
-	smartrg,sdg-8622|\
-	smartrg,sdg-8632|\
-	smartrg,sdg-8733|\
-	smartrg,sdg-8734)
-		CI_KERNPART="kernel"
-		CI_ROOTPART="rootfs"
-		emmc_do_upgrade "$1"
-		;;
-	asus,rt-ax59u|\
-	asus,tuf-ax4200|\
-	asus,tuf-ax6000)
+	asus,tuf-ax4200)
 		CI_UBIPART="UBI_DEV"
 		CI_KERNPART="linux"
 		nand_do_upgrade "$1"
 		;;
 	bananapi,bpi-r3|\
-	bananapi,bpi-r3-mini|\
 	bananapi,bpi-r4|\
-	bananapi,bpi-r4-poe|\
-	jdcloud,re-cp-03|\
-	mediatek,mt7988a-rfb|\
-	openwrt,one)
+	bananapi,bpi-r4-poe)
 		[ -e /dev/fit0 ] && fitblk /dev/fit0
 		[ -e /dev/fitrw ] && fitblk /dev/fitrw
 		bootdev="$(fitblk_get_bootdev)"
 		case "$bootdev" in
-		mmcblk*)
+		mmc*)
 			EMMC_KERN_DEV="/dev/$bootdev"
 			emmc_do_upgrade "$1"
 			;;
@@ -107,89 +42,14 @@ platform_do_upgrade() {
 			;;
 		esac
 		;;
-	cmcc,rax3000m)
-		case "$(cmdline_get_var root)" in
-		/dev/mmc*)
-			CI_KERNPART="production"
-			emmc_do_upgrade "$1"
-			;;
-		*)
-			CI_KERNPART="fit"
-			nand_do_upgrade "$1"
-			;;
-		esac
-		;;
-	cudy,re3000-v1|\
-	cudy,wr3000-v1|\
-	yuncore,ax835)
-		default_do_upgrade "$1"
-		;;
+	cmcc,rax3000m-emmc|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt6000|\
-	glinet,gl-x3000|\
-	glinet,gl-xe3000)
+	hf,m7986r1-emmc|\
+	jdcloud,re-cs-05)
 		CI_KERNPART="kernel"
 		CI_ROOTPART="rootfs"
 		emmc_do_upgrade "$1"
-		;;
-	h3c,magic-nx30-pro|\
-	jcg,q30-pro|\
-	mediatek,mt7981-rfb|\
-	netcore,n60|\
-	qihoo,360t7|\
-	xiaomi,mi-router-ax3000t-ubootmod|\
-	xiaomi,mi-router-wr30u-ubootmod)
-		CI_KERNPART="fit"
-		nand_do_upgrade "$1"
-		;;
-	mercusys,mr90x-v1|\
-	tplink,re6000xd)
-		CI_UBIPART="ubi0"
-		nand_do_upgrade "$1"
-		;;
-	nokia,ea0326gmp|\
-	tplink,tl-xdr4288|\
-	tplink,tl-xdr6086|\
-	tplink,tl-xdr6088|\
-	xiaomi,redmi-router-ax6000-ubootmod)
-		[ -e /dev/fit0 ] && fitblk /dev/fit0
-		[ -e /dev/fitrw ] && fitblk /dev/fitrw
-		CI_KERNPART="fit"
-		nand_do_upgrade "$1"
-		;;
-	ubnt,unifi-6-plus)
-		CI_KERNPART="kernel0"
-		EMMC_ROOT_DEV="$(cmdline_get_var root)"
-		emmc_do_upgrade "$1"
-		;;
-	xiaomi,mi-router-ax3000t|\
-	xiaomi,mi-router-wr30u-stock|\
-	xiaomi,redmi-router-ax6000-stock)
-		CI_KERN_UBIPART=ubi_kernel
-		CI_ROOT_UBIPART=ubi
-		nand_do_upgrade "$1"
-		;;
-        zyxel,ex5601-t0-ubootmod)
-		CI_KERNPART="fit"
-		CI_ROOTPART="ubi_rootfs"
-                nand_do_upgrade "$1"
-                ;;
-	unielec,u7981-01*)
-		local rootdev="$(cmdline_get_var root)"
-		rootdev="${rootdev##*/}"
-		rootdev="${rootdev%p[0-9]*}"
-		case "$rootdev" in
-		mmc*)
-			CI_ROOTDEV="$rootdev"
-			CI_KERNPART="kernel"
-			CI_ROOTPART="rootfs"
-			emmc_do_upgrade "$1"
-			;;
-		*)
-			CI_KERNPART="fit"
-			nand_do_upgrade "$1"
-			;;
-		esac
 		;;
 	*)
 		nand_do_upgrade "$1"
@@ -207,10 +67,8 @@ platform_check_image() {
 
 	case "$board" in
 	bananapi,bpi-r3|\
-	bananapi,bpi-r3-mini|\
 	bananapi,bpi-r4|\
-	bananapi,bpi-r4-poe|\
-	cmcc,rax3000m)
+	bananapi,bpi-r4-poe)
 		[ "$magic" != "d00dfeed" ] && {
 			echo "Invalid image type."
 			return 1
@@ -219,7 +77,7 @@ platform_check_image() {
 		;;
 	*)
 		nand_do_platform_check "$board" "$1"
-		return $?
+		return 0
 		;;
 	esac
 
@@ -228,36 +86,20 @@ platform_check_image() {
 
 platform_copy_config() {
 	case "$(board_name)" in
-	cmcc,rax3000m)
+	bananapi,bpi-r3|\
+	bananapi,bpi-r4|\
+	bananapi,bpi-r4-poe)
 		case "$(cmdline_get_var root)" in
 		/dev/mmc*)
 			emmc_copy_config
 			;;
 		esac
 		;;
-	bananapi,bpi-r3|\
-	bananapi,bpi-r3-mini|\
-	bananapi,bpi-r4|\
-	bananapi,bpi-r4-poe)
-		case "$(fitblk_get_bootdev)" in
-		mmcblk*)
-			emmc_copy_config
-			;;
-		esac
-		;;
-	acer,predator-w6|\
+	cmcc,rax3000m-emmc|\
 	glinet,gl-mt2500|\
 	glinet,gl-mt6000|\
-	glinet,gl-x3000|\
-	glinet,gl-xe3000|\
-	jdcloud,re-cp-03|\
-	smartrg,sdg-8612|\
-	smartrg,sdg-8614|\
-	smartrg,sdg-8622|\
-	smartrg,sdg-8632|\
-	smartrg,sdg-8733|\
-	smartrg,sdg-8734|\
-	ubnt,unifi-6-plus)
+	hf,m7986r1-emmc|\
+	jdcloud,re-cs-05)
 		emmc_copy_config
 		;;
 	esac
@@ -267,15 +109,8 @@ platform_pre_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
-	asus,rt-ax59u|\
-	asus,tuf-ax4200|\
-	asus,tuf-ax6000)
+	asus,tuf-ax4200)
 		asus_initial_setup
-		;;
-	xiaomi,mi-router-ax3000t|\
-	xiaomi,mi-router-wr30u-stock|\
-	xiaomi,redmi-router-ax6000-stock)
-		xiaomi_initial_setup
 		;;
 	esac
 }
